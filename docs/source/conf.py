9c83312e138ca1b69aef54959af867db6aaffaf3
harper <harper@wistia.com>
2023-07-12 13:02:07 -0400

    commit3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'HarperDocs'
-copyright = '2023, ArrHarper'
-author = 'Harper@Wistia'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
 release = '0.1'
 version = '0.1.0'
